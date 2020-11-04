5033253018a594008e279965d0a49e31216a6a7e
David Fernández <Dafs@localhost.localdomain>
2020-11-04 02:08:08 -0300

    Agregando clientes  Betzabe y Matias

--- app/src/main/java/com/example/evaluacion2/PrestamosAct.java
+++ app/src/main/java/com/example/evaluacion2/PrestamosAct.java
@@ -24,7 +24,7 @@ public class PrestamosAct extends AppCompatActivity {
         spinClientes = (Spinner)findViewById(R.id.spnClientes);
         spinCredito = (Spinner)findViewById(R.id.spnCredito);
         hashclientes = (HashMap<String, Integer>) getIntent().getSerializableExtra("Clientes");
-        String [] listadoClientes = {"(Seleccione Cliente)","Axel","Roxana"};
+        String [] listadoClientes = {"(Seleccione Cliente)","Axel","Roxana","Betzabe","Matias"};
         String [] listadoCreditos = {"(Seleccione Credito)","Credito Hipotecario","Credito Automotriz"};
         ArrayAdapter<String> adaptClientes = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,listadoClientes);
         spinClientes.setAdapter(adaptClientes);
@@ -33,22 +33,22 @@ public class PrestamosAct extends AppCompatActivity {
     }
     public void CalcularPrestamo(View view){
         String resultado;
-        if(spinCredito.getSelectedItemPosition() == 1){
+        if(spinCredito.getSelectedItemPosition() == 1 && (spinClientes.getSelectedItemPosition() == 1 || spinClientes.getSelectedItemPosition() == 2) ){
             resultado = ("Su saldo es = $" + String.valueOf((hashclientes.get(spinClientes.getSelectedItem().toString())+creditos[0])));
             edResultado.setText(resultado);
         }
-        else if(spinCredito.getSelectedItemPosition() == 2){
+        else if(spinCredito.getSelectedItemPosition() == 2 && (spinClientes.getSelectedItemPosition() == 1 || spinClientes.getSelectedItemPosition() == 2)){
             resultado = ("Su saldo es = $" + String.valueOf((hashclientes.get(spinClientes.getSelectedItem().toString())+creditos[1])));
             edResultado.setText(resultado);
         }
     }
     public void CalcularDeuda(View view){
         String resultado;
-        if(spinCredito.getSelectedItemPosition() == 1){
+        if(spinCredito.getSelectedItemPosition() == 1 && (spinClientes.getSelectedItemPosition() == 1 || spinClientes.getSelectedItemPosition() == 2)){
             resultado = ("El valor de sus cuotas es = $" + String.valueOf((hashclientes.get(spinClientes.getSelectedItem().toString())+creditos[0]) /12 ) + "\nA 12 cuotas" );
             edResultado.setText(resultado);
         }
-        else if(spinCredito.getSelectedItemPosition() == 2){
+        else if(spinCredito.getSelectedItemPosition() == 2 && (spinClientes.getSelectedItemPosition() == 1 || spinClientes.getSelectedItemPosition() == 2)){
             resultado = ("El valor de sus cuotas es = $" + String.valueOf((hashclientes.get(spinClientes.getSelectedItem().toString())+creditos[1])/8) + "\nA 8 cuotas");
             edResultado.setText(resultado);
         }
